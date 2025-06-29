c249466e565cebc5eaa2bfba6b30ef14fd65d581
Muhammad Usman <muhammadusmanramzan586@gmail.com>
2025-06-29 11:39:24 +0500

    fixing bugs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: import.meta.env.VITE_BASE_PATH || "/fyp-frontend",
   server: {
     proxy: {
       "/api/": "https://fyp-backend-p1jf.onrender.com",
